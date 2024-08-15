92632f4ee0f4543f2a24707cc2555a215bd15539
Justin Antunes <justinantz1100@gmail.com>
2024-08-15 14:42:40 -0400

    feat: add new card modal, refactor: modal variable & foreach function body

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-let initialCards = [
+const initialCards = [
   {
     name: "Yosemite Valley",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
@@ -29,7 +29,7 @@ let initialCards = [
 /*                                  Elements                                  */
 /* -------------------------------------------------------------------------- */
 const editButton = document.querySelector(".profile__edit-button");
-const modal = document.querySelector(".modal");
+const editProfileModal = document.querySelector("#edit-modal");
 const closeButton = document.querySelector(".modal__close");
 const profileName = document.querySelector(".profile__name");
 const profileDescription = document.querySelector(".profile__description");
@@ -37,7 +37,7 @@ const profileNameInput = document.querySelector("#profile-name-input");
 const profileDescriptionInput = document.querySelector(
   "#profile-description-input"
 );
-const profileEditForm = modal.querySelector(".modal__form");
+const profileEditForm = editProfileModal.querySelector(".modal__form");
 const cardListEl = document.querySelector(".cards__list");
 const cardTemplate =
   document.querySelector("#card-template").content.firstElementChild;
@@ -45,16 +45,16 @@ const cardTemplate =
 /*                                  Functions                                 */
 /* -------------------------------------------------------------------------- */
 function toggleModal() {
-  modal.classList.toggle("modal_opened");
+  editProfileModal.classList.toggle("modal_opened");
 }
 
 function getCardElement(cardData) {
   const cardElement = cardTemplate.cloneNode(true);
   const cardImageEl = cardElement.querySelector(".card__image");
   const cardTitleEl = cardElement.querySelector(".card__title");
-  cardImageEl.src = cardData.link
-  cardImageEl.alt = cardData.name
-  cardTitleEl.textContent = cardData.name
+  cardImageEl.src = cardData.link;
+  cardImageEl.alt = cardData.name;
+  cardTitleEl.textContent = cardData.name;
   return cardElement;
 }
 
@@ -82,6 +82,5 @@ closeButton.addEventListener("click", toggleModal);
 profileEditForm.addEventListener("submit", handleProfileEditSubmit);
 
 initialCards.forEach((cardData) => {
-  const cardElement = getCardElement(cardData);
-  cardListEl.prepend(cardElement);
+  cardListEl.prepend(getCardElement(cardData));
 });
